f4076b89ba64bcaa7c63b240df208613a51549a7
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-03-02 21:30:41 +0530

    Added OTP Signup page

---
+++ week-13/src/pages/OTPSignup.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function OTPSignup() {
+  return (
+    <div className='bg-primary h-screen w-screen flex justify-center items-center'>
+      <h1 className='text-white text-xl'>Enter the OTP</h1>
+      <div className='flex gap-x-2'>
+
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      </div>
+    </div>
+  )
+}
+
+export default OTPSignup
