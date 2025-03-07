8e5c5ac72b4d61e2315f0ad0ce7b463127763d65
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-03-07 23:52:23 +0530

    Added new page

---
+++ week-13/src/pages/Assignments.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SideBar() {
+  return (
+    <div className=' flex h-full w-full'>
+      <div className=' bg-red-700 w-0 md:w-[25%] transition-all duration-500 ease-in'>Sidebar</div>
+      <div className='w-full h-screen md:w-[75%] bg-blue-700 text-white'>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default SideBar
