7836bc60e6f62111976d775fc689e4f1c2072f9a
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-03-09 00:19:20 +0530

    Completed the tailwind class

--- week-13/src/App.jsx
+++ week-13/src/App.jsx
@@ -1,29 +1,76 @@
 import "./App.css";
-// import { useState } from "react";
+import { useState } from "react";
+import MenuBar from "./Components/Icons/MenuBar";
 // import Assignments from "./pages/Assignments";
 // import OTPSignup from "./pages/OTPSignup";
 
 function App() {
+  const [sidebar, setsidebar] = useState(true);
   return (
     <>
-      {/* <OTPSignup /> */}
-      {/* <div className='flex w-screen h-screen'>
+      <div className="flex h-full w-full">
+        <Sidebar sidebar={sidebar} setsidebar={setsidebar} />
+        <MainContent />
+      </div>
+    </>
+  );
+}
 
-      <Assignments/>
-      </div> */}
-      <div className="w-screen h-screen flex justify-center flex-col items-center bg-white dark:bg-gray-900 text-black dark:text-white">
-        <h1 className="text-xl font-bold text-center py-4 cursor-pointer">
-          {" "}
-          Aditya Kumar Gupta{" "}
-        </h1>
-        <button
+function Sidebar({ setsidebar, sidebar }) {
+  return (
+    <>
+      <div
+        className={`bg-blue-500   h-screen ${
+          sidebar ? " w-full md:w-[15%]  " : "w-fit "
+        } p-4 text-white  fixed top-0 left-0 z-10 md:relative `}
+      >
+        <div
+          className=" cursor-pointer px-3 py-3"
           onClick={() => {
-            document.querySelector("html").classList.toggle("dark");
+            setsidebar(!sidebar);
           }}
-          className="bg-accent p-3 cursor-pointer hover:bg-red-700 dark:hover:bg-blue-900 dark:bg-blue-600 font-bold rounded-lg"
         >
-          Toggle Button
-        </button>
+          <MenuBar />
+        </div>
+        {sidebar && (
+          <ul className="list-none gap-2">
+            <li className="font-medium cursor-pointer hover:bg-sky-400 hover:rounded-md px-3 py-3">
+              Home
+            </li>
+            <li className="font-medium cursor-pointer hover:bg-sky-400 hover:rounded-md px-3 py-3">
+              Profile
+            </li>
+            <li className="font-medium cursor-pointer hover:bg-sky-400 hover:rounded-md px-3 py-3">
+              Settings
+            </li>
+            <li className="font-medium cursor-pointer hover:bg-sky-400 hover:rounded-md px-3 py-3">
+              Logout
+            </li>
+          </ul>
+        )}
+      </div>
+    </>
+  );
+}
+
+function MainContent() {
+  return (
+    <>
+      <div className=" w-full ">
+        {/* Black Bar */}
+        <div className="h-[15%] bg-black w-full"></div>
+        {/* Appointentment Section */}
+        <div className="grid grid-cols-8 gap-7 h-[85%] p-5">
+          <div className="border border-slate-600 shadow shadow-slate-400  rounded-2xl  h-85 -translate-y-16 col-span-8 md:col-span-2 text-black">
+            Gir Card
+          </div>
+          <div className="border border-slate-600 shadow shadow-slate-400   rounded-2xl h-85  md:col-span-4  col-span-8text-black">
+            Gir Card
+          </div>
+          <div className="border border-slate-600 shadow shadow-slate-400  rounded-2xl  h-85  md:col-span-2 col-span-8 text-black">
+            Gir Card
+          </div>
+        </div>
       </div>
     </>
   );
--- week-13/src/Components/Sidebar.jsx
+++ week-13/src/Components/Sidebar.jsx
@@ -5,7 +5,7 @@ function SideBar() {
     <div className=' flex h-full w-full'>
       <div className=' bg-red-700 w-0 md:w-[25%] transition-all duration-500 ease-in'>Sidebar</div>
       <div className='w-full h-screen md:w-[75%] bg-blue-700 text-white'>
-        <div></div>
+        <div>Content</div>
       </div>
     </div>
   )
