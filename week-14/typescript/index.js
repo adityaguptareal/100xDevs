20329e3a335ee79866701b1f3faa43b9690b93e6
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-03-10 23:58:49 +0530

    Changes

---
+++ week-14/typescript/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function sum(a, b) {
+    console.log("sum is", a + b);
+}
+sum(10, "Aditya");
---
+++ week-14/typescript/index.ts
@@ -0,0 +1,4 @@
+function sum(a: number, b: number) {
+    console.log("sum is", a + b);
+}
+sum(10, 20);
