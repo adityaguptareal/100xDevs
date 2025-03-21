dcc602ca2898a680283e144ecb19028baa9c2792
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-03-21 23:55:09 +0530

    TypeScipt Compilation

--- week-14/typescript/index.js
+++ week-14/typescript/index.js
@@ -1,5 +1,18 @@
 "use strict";
-function sum(a, b) {
-    console.log("sum is", a + b);
+// function delayedCall(fn:()=>void){
+//     setTimeout(fn,4000)
+// }
+function greet(user) {
+    return (`Hello ${user.name} your mobile number is ${user.mobileNumber} and marks is ${user.marks} and the address is ${user.address}`);
 }
-sum(10, "Aditya");
+// let user: {
+//     firstName: string
+//     lastName: string,
+//     mobileNo:string
+// } = {
+//     firstName: "adity",
+//     lastName: "Yadav",
+//     mobileNo: "9153649577"
+// }
+let greeting = greet({ name: "Aditya", mobileNumber: 917234657, marks: 90, address: "Bangalore" });
+console.log(greeting);
