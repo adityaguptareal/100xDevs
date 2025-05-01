399781b3b0a4912aa3df8ffc822e70f8625d97fd
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-05-01 16:45:05 +0530

    dist folder removed

--- week-15/brainly/dist/config/credenitals.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dbConnectionString = exports.jwt_Secret = void 0;
-exports.jwt_Secret = "1232@";
-exports.dbConnectionString = "mongodb+srv://adityaguptareal:dbadityaguptareal@cluster0.3rcjj.mongodb.net/brainly";
