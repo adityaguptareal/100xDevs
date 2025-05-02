5286daf5c4fb6c389b0df160ba9a65741c9ee760
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-05-02 12:49:21 +0530

    added share endpoints

---
+++ week-15/brainly/Backend/src/models/db.ts
@@ -0,0 +1,33 @@
+import { hash } from "bcrypt";
+import mongoose, { model, mongo } from "mongoose";
+
+// user schema
+const userSchema = new mongoose.Schema({
+    username:{type:String, required:true, unique:true},
+    password:{type:String, required:true},
+    email:{type:String, required:true, unique:true},
+})
+
+export const userModel= mongoose.model("userData", userSchema);
+
+// content schema
+
+const contentSchema= new mongoose.Schema({
+    title:{type:String},
+    link:{type:String},
+    tags:[{type:mongoose.Types.ObjectId,ref:"Tag"}],
+    userId:{type:mongoose.Types.ObjectId,ref:"userData"}
+})
+
+export const contentModel=mongoose.model("content",contentSchema)
+
+
+// link schema
+
+const linkSchema= new mongoose.Schema({
+    hash:{type:String,unique:true,require:true},
+    userId:{type:mongoose.Types.ObjectId,ref:"userData",unique:true}
+    
+})
+
+export const LinkModel= mongoose.model("contentLinks",linkSchema)
