549cde227e3fb34e829094a999e37baf8d9cab84
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-05-04 21:49:52 +0530

    Added sidebar styling

--- week-15/brainly/Frontend/src/Components/ui/Sidebar.tsx
+++ week-15/brainly/Frontend/src/Components/ui/Sidebar.tsx
@@ -1,7 +1,20 @@
+import { Brain, CircleEllipsis, FileMinus, Twitter, Youtube, } from "lucide-react";
+import { SidebarItems } from "./SidebarItems";
+
 export function Sidebar() {
-    return(
+    return (
         <div className="h-screen bg-white absolute left-0 right-0  border-r border-gray-200 w-64 flex flex-col gap-4 p-4">
-Hi There
+            <div id="logo" className="flex justify-start items-center gap-2 text-2xl font-bold text-purple-700">
+            <Brain size={40} color="#5046E4" /> SecondBrain
+
+            </div>
+            <div className="flex flex-col gap-4 mt-4">
+
+            <SidebarItems icon={ <Twitter />} text="Twitter" />
+            <SidebarItems icon={<Youtube />} text="Youtube" />
+            <SidebarItems icon={<FileMinus />} text="Docuemts" />
+            <SidebarItems icon={ <CircleEllipsis />} text="Others" />
+            </div>
         </div>
     )
 }
