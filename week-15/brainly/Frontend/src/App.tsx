49e3c441ac93402023a3b6fa2ed26ba77c1cc04b
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-05-05 07:49:18 +0530

    Added routes

--- week-15/brainly/Frontend/src/App.tsx
+++ week-15/brainly/Frontend/src/App.tsx
@@ -1,44 +1,26 @@
 
 import './App.css'
-import { Plus } from './Components/ui/Icons/Plus.tsx'
-import { Button } from './Components/ui/Button'
-import { Share } from './Components/ui/Icons/Share'
-import { Card } from './Components/ui/Card.tsx'
-import { CreateContentModel } from './Components/ui/CreateContentModel.tsx'
-import { useState } from 'react'
-import { Sidebar } from './Components/ui/Sidebar.tsx'
+import Dashboard from './Pages/Dashboard'
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup'
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast';
+
 function App() {
 
-  const [modalopen, setModalopen] = useState(false)
 
   return (
-   <div className='flex bg-slate-50 min-h-screen gap-4 p-4'>
-    <Sidebar/>
-    <div className=''>
-      <div className='flex gap-3 items-center justify-end'>
-    <Button
-      variant='primary'
-      size='medium'
-      text='Add Content' onClick={()=>{setModalopen(true)}} startIcon={<Plus size='md'/>} />
+    <>
+      <Router>
+        <Routes>
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+        <Toaster />
+      </Router>
 
-      <Button
-      variant='secondary'
-      size='medium'
-      text='Share brain' startIcon={<Share size='md'/>} />
-      </div>
-
-
-    <div className='grid grid-cols-4 gap-4 pt-10 ml-64'>
-    <Card type='youtube' title='Gaming post' link={"https://www.youtube.com/watch?v=YvBaRWzOyqM"}/>
-    <Card type='twitter' title='Vibe Coding' link={"https://x.com/mannupaaji/status/1917960933558620508"}/>
-    <Card type='twitter' title='Web 3 Hackathon' link={"https://x.com/shubhxm_mishra/status/1918431355483848730"}/>
-      </div>
-    {modalopen && <CreateContentModel setModalopen={setModalopen}/>}
-
-      
-    
-      </div>
-   </div>
+    </>
   )
 }
 
