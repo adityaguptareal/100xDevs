9542829266dfb23242886915c22b773c4e3aa395
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-05-06 22:32:51 +0530

    Added cross icon

---
+++ week-15/brainly/Frontend/src/Components/ui/Icons/Cross.tsx
@@ -0,0 +1,14 @@
+
+interface iconSize{
+    size:'size-2' | 'size-4' | 'size-6' | 'size-8'
+}
+export function Cross({size}:iconSize) {
+    return (
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={size}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+
+        </div>
+    )
+}
