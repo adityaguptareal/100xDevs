e7c3f88a4144c20c114fa8f5244edd72cf44c292
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-05-21 07:32:51 +0530

    Get more info from body

--- week-17/dist/index.js
+++ week-17/dist/index.js
@@ -1,30 +1,35 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
 const pgClients = new pg_1.Client("postgresql://neondb_owner:npg_b9xqAklcX8js@ep-white-cell-a1q5cnve-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require");
-// Another Way
-// const pgClients = new Client({
-//     user: "neondb_owner",
-//     password: "npg_b9xqAklcX8js",
-//     port: 5432,
-//     host: "ep-white-cell-a1q5cnve-pooler.ap-southeast-1.aws.neon.tech",
-//     database: "neondb"
-// })
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield pgClients.connect();
-        // const response= await pgClients.query("SELECT * FROM Users")
-        const response = yield pgClients.query("UPDATE users SET password='Ak9955594@' WHERE email='adityaguptapro@gmailcom'");
-        console.log(response.rows);
-    });
-}
-main();
+pgClients.connect();
+app.use(express_1.default.json());
+app.post("/signup", (req, res) => {
+    const { username, email, password } = req.body;
+    // Another Way
+    // const pgClients = new Client({
+    //     user: "neondb_owner",
+    //     password: "npg_b9xqAklcX8js",
+    //     port: 5432,
+    //     host: "ep-white-cell-a1q5cnve-pooler.ap-southeast-1.aws.neon.tech",
+    //     database: "neondb"
+    // })
+    // const response= await pgClients.query("SELECT * FROM Users")
+    // const response= await pgClients.query("UPDATE users SET password='Ak9955594@' WHERE email='adityaguptapro@gmail.com'")
+    // Can cause SQL Injection Problem
+    // const insertQuery = `INSERT INTO users (username,email,password) VALUES (${username},{$email},${password})`;
+    try {
+        const insertQuery = `INSERT INTO users (username,email,password) VALUES ($1,$2,$3)`;
+        const responseQuery = pgClients.query(insertQuery, [username, email, password]);
+        console.log(responseQuery);
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
