2c6d0e69617050ab2e428b13432eecc226fe6913
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-06-01 00:53:47 +0530

    Learn CRUD in prisma

--- week-18/Recap/src/index.ts
+++ week-18/Recap/src/index.ts
@@ -1,1 +1,53 @@
-console.log("Hello, World!");
+import {PrismaClient} from "@prisma/client"
+// import { PrismaClient } from "./generated/prisma"
+const client = new PrismaClient()
+
+async function main() {
+
+
+// Inserting Enteries 
+
+
+    // await client.users.create({
+    //     data: {
+    //         email: "adityagupcxcsdtapro@gmail.com",
+    //         age: 23,
+    //         city: "Noidacsdc",
+    //         password: "Ak184cds0319@",
+    //     }
+    // })
+
+
+
+    // Deleting Entries
+
+
+    //  await client.users.delete({
+    //     where: {
+    //         id: 2
+    //     }
+    // })
+
+
+
+// updating the records
+
+
+    // await client.users.update({
+    //     where: {
+    //         email:"adityaguptapro@gmail.com"
+    //     },
+    //     data:{
+    //         email:"adityakumargupta@gmail.com"
+    //     }
+    // })
+
+
+
+// Counting Records in the  
+
+
+//  const users=await client.users.count()
+//  console.log(users)   
+}
+main()
