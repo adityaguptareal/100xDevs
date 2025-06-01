a696d900fec5812506ab22323eac45968a256818
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-06-01 15:03:46 +0530

    Added express in the app

--- week-18/Recap/src/index.ts
+++ week-18/Recap/src/index.ts
@@ -2,92 +2,112 @@ import { PrismaClient } from "./generated/prisma"
 import express from "express"
 
 const app = express()
-const port=3000
+const port = 3000
 const client = new PrismaClient()
 
-async function main() {
 
+app.get("/", (req, res) => {
+    res.status(200).json({ "message": "Hello World" });
+})
 
-    // Inserting Enteries 
-
-
-    // await client.users.create({
-    //     data: {
-    //         email: "adityagupcxcsdtapro@gmail.com",
-    //         age: 23,
-    //         city: "Noidacsdc",
-    //         password: "Ak184cds0319@",
-    //     }
-    // })
-
-
-
-    // Deleting Entries
-
-
-    //  await client.users.delete({
-    //     where: {
-    //         id: 2
-    //     }
-    // })
-
-
-
-    // updating the records
-
-
-    // await client.users.update({
-    //     where: {
-    //         email:"adityaguptapro@gmail.com"
-    //     },
-    //     data:{
-    //         email:"adityakumargupta@gmail.com"
-    //     }
-    // })
-
-
-
-    // Counting Records in the  
-
-
-    //  const users=await client.users.count()
-    //  console.log(users)  
-
-
-
-
-
-
-
-    // await client.todo.create({
-    //     data: {
-    //         title: "Test todo",
-    //         description: "This is test data",
-    //         done: false,
-    //         time: new Date,
-    //         userId:1
-    //     }
-    // })
-
-
-
-    // getting data by referenceing
-
-    
-
-    const userTodo = await client.users.findFirst({
+app.get("/user/todos/:id", async (req, res) => {
+    const id = req.params.id
+    const userTodo = await client.todo.findMany({
         where: {
-            id: 1
-        },
-        select: {
-            todo: true
+            userId: Number(id)
         }
     })
+    res.status(200).json({ todos: userTodo })
+})
+
+app.listen(port, () => {
+    console.log("App is running at", port)
+})
+
+
+// async function main() {
+
+
+//     // Inserting Enteries 
+
+
+//     // await client.users.create({
+//     //     data: {
+//     //         email: "adityagupcxcsdtapro@gmail.com",
+//     //         age: 23,
+//     //         city: "Noidacsdc",
+//     //         password: "Ak184cds0319@",
+//     //     }
+//     // })
 
 
 
-    console.log(userTodo)
+//     // Deleting Entries
 
 
-}
-main()
+//     //  await client.users.delete({
+//     //     where: {
+//     //         id: 2
+//     //     }
+//     // })
+
+
+
+//     // updating the records
+
+
+//     // await client.users.update({
+//     //     where: {
+//     //         email:"adityaguptapro@gmail.com"
+//     //     },
+//     //     data:{
+//     //         email:"adityakumargupta@gmail.com"
+//     //     }
+//     // })
+
+
+
+//     // Counting Records in the  
+
+
+//     //  const users=await client.users.count()
+//     //  console.log(users)  
+
+
+
+
+
+
+
+//     // await client.todo.create({
+//     //     data: {
+//     //         title: "Test todo",
+//     //         description: "This is test data",
+//     //         done: false,
+//     //         time: new Date,
+//     //         userId:1
+//     //     }
+//     // })
+
+
+
+//     // getting data by referenceing
+
+
+
+//     const userTodo = await client.users.findFirst({
+//         where: {
+//             id: 1
+//         },
+//         select: {
+//             todo: true
+//         }
+//     })
+
+
+
+//     console.log(userTodo)
+
+
+// }
+// main()
