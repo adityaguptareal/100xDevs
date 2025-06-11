d2800d57169ebbf8b95fd47e88a8378247c6b44d
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-06-12 00:05:49 +0530

    updated directory

---
+++ week-20/app/blog/[...blogId]/page.tsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export default async function blog({params}:any) {
+    
+    const postId= (await params).blogId
+    return(
+        <div>
+            {JSON.stringify(postId)}
+        </div>
+    )
+}
