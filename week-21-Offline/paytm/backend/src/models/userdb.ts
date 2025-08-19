bb074b594157c9f7df4d8222f3f495e883752b14
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-08-19 15:10:07 +0530

    Added account schema

--- week-21-Offline/paytm/backend/src/models/userdb.ts
+++ week-21-Offline/paytm/backend/src/models/userdb.ts
@@ -3,11 +3,26 @@ const Schema = mongoose.Schema
 
 const userSchema = new mongoose.Schema({
     createdAt: { type: String, unique: true, default: Date.now },
-    email: { type: String, unique: true, require: true },
-    password: { type: String, require: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
     firstName: { type: String },
     lastName: { type: String },
 
 })
 
+const accountSchema = new mongoose.Schema({
+    userid: {
+        type: mongoose.Schema.Types.ObjectId, ref: "users", required: true
+    },
+    balance: {
+        type: Number,
+        required: true,
+    },
+    lastUpdated: {
+        type: String,
+        default: Date.now
+    }
+})
+
 export const userModel = mongoose.model("users", userSchema)
+export const accountModel = mongoose.model("accounts", accountSchema)
