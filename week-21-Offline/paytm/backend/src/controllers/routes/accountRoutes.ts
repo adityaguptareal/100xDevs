d8f0509207b72b1ed37e03f520fe57cba8254d51
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-08-19 15:08:38 +0530

    Configured Balance Routes

--- week-21-Offline/paytm/backend/src/controllers/routes/accountRoutes.ts
+++ week-21-Offline/paytm/backend/src/controllers/routes/accountRoutes.ts
@@ -1,11 +1,32 @@
-import { Router } from "express";
-const accountRoutes=Router()
+import { Router, Request, Response } from "express"
+import userMiddleware from "../middleware/usermiddleware";
+import { accountModel, userModel } from "../../models/userdb";
 
-accountRoutes.get("/",(req,res)=>{
+const accountRoutes = Router()
+
+accountRoutes.get("/", (req, res) => {
     res.send("Account Routes work properly")
 })
-accountRoutes.get("/balance",(req,res)=>{
-    res.send("Balance Routes work properly")
+
+accountRoutes.get("/balance", userMiddleware, async (req: Request, res: Response) => {
+    try {
+        // @ts-ignore
+        const checkAccount = await userModel.findById(req.userId)
+        if (!checkAccount) {
+            res.status(400).json({ message: "User Does not Exist" })
+        }
+
+        // @ts-ignore
+        const account = await accountModel.findOne({
+            // @ts-ignore
+            userid: req.userId
+        })
+
+        res.json({ accountBalance: account?.balance || 0 })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "Something went wrong" })
+    }
 })
 
-export {accountRoutes}
+export { accountRoutes }
